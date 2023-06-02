415ded42a4aaaba0d240f9e6555a9e64d9cb19db
vinothr <vinodhrajalingam@gmail.com>
2023-06-02 11:14:02 +0530

    Import conflicts resolved

--- src/main/java/com/srv/streams/StreamsApplication.java
+++ src/main/java/com/srv/streams/StreamsApplication.java
@@ -1,6 +1,5 @@
 package com.srv.streams;
 
-import com.srv.streams.beans.Departments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
